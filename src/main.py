cc97da7a12818e4ec39ccbb756b3af59edaee051
Alexey <alexey.kostenyu@gmail.com>
2023-07-11 12:14:46 +0400

    1 commit - Get abstractmethod anв apiHH

---
+++ src/main.py
@@ -0,0 +1,58 @@
+from abc import ABC, abstractmethod
+import requests
+
+class AbstractJobSiteAPI(ABC):
+    """
+    Абстрактный класс для работы с API сайтов с вакансиями.
+    """
+
+    @abstractmethod
+    def connect(self):
+        """
+        Метод для подключения к API сайта.
+        """
+        pass
+
+    @abstractmethod
+    def get_jobs(self, search_params):
+        """
+        Метод для получения вакансий с сайта.
+
+        :param search_params: Параметры поиска вакансий.
+        :return: Список вакансий.
+        """
+        pass
+
+
+
+
+
+class HhRuAPI(AbstractJobSiteAPI):
+    """
+    Класс для работы с API сайта hh.ru.
+    """
+
+    def __init__(self, api_url):
+        self.api_url = api_url
+
+    def connect(self):
+        """
+        Подключение к API сайта hh.ru.
+        """
+        # Ничего не делаем, так как подключение к API hh.ru прописать отдельно.
+        pass
+
+    def get_jobs(self, search_params):
+        """
+        Получение вакансий с сайта hh.ru.
+
+        :param search_params: Параметры поиска вакансий.
+        :return: Список вакансий.
+        """
+        # Отправляем запрос к API сайта hh.ru и получаем список вакансий.
+        response = requests.get(self.api_url, params=search_params)
+        jobs = response.json()
+
+        return jobs
+
+
