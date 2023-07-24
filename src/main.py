434e54a4bd4980b6d70cba158c86e7fedce39fba
Alexey <alexey.kostenyu@gmail.com>
2023-07-24 23:18:35 +0400

    10 commit - fixed another bugs

--- src/main.py
+++ src/main.py
@@ -9,18 +9,9 @@ class AbstractJobSiteAPI(ABC):
     Абстрактный класс для работы с API сайтов с вакансиями.
     """
     @abstractmethod
-    def connect(self):
-        """
-        Метод для подключения к API сайта.
-        """
-        pass
-
-    @abstractmethod
     def get_jobs(self, vacancy):
         """
         Метод для получения вакансий с сайта.
-        Параметры поиска вакансий.
-        :return: Список вакансий.
         """
         pass
 
@@ -34,15 +25,15 @@ class HhRuAPI(AbstractJobSiteAPI):
         """
         # Отправляем запрос к API сайта hh.ru и получаем список вакансий.
         hh_dict = {}
-            for page in range(0, 3):
-                params = {
-                    "text": vacancy,
-                    "per_page": 100,
-                    "page": page,
-                }
-                response = requests.get("https://api.hh.ru/vacancies", params=params).json()
-                hh_dict.update(response)
-            return hh_dict
+        for page in range(0, 3):
+            params = {
+                "text": vacancy,
+                "per_page": 100,
+                "page": page,
+            }
+            response = requests.get("https://api.hh.ru/vacancies", params=params).json()
+            hh_dict.update(response)
+        return hh_dict
 
 class SuperJobRuAPI(AbstractJobSiteAPI):
     """
